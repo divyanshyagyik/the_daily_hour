cf72bc00883ac18700a66911e960115f7cdf9f1e
divyanshyagyik <divyanshyagyik1189@gmail.com>
2023-09-27 00:48:50 +0530

    first

---
+++ android/app/src/main/java/com/example/thedailyhour/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.thedailyhour;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/home.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController searchController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("THE DAILY HOUR"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            //Search Wala Container
+
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24)),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    if ((searchController.text).replaceAll(" ", "") ==
+                        "") {
+                      print("Blank search");
+                    } else {
+                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                    }
+                  },
+                  child: Container(
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.blueAccent,
+                    ),
+                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (value){
+                      print(value);
+                    },
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Health"),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
